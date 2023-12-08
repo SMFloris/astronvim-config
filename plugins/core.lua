bc213cf968b3154cc317e34489c3459fdbf23f1b
Floris Stoica <floris@nusunt.eu>
2023-12-09 01:35:19 +0200

    enable plugin

--- plugins/core.lua
+++ plugins/core.lua
@@ -21,10 +21,6 @@ return {
     end,
   },
   {
-    "echasnovski/mini.completion",
-    event = "LspAttach"
-  },
-  {
     "s1n7ax/nvim-window-picker",
     opts = function(_, opts)
       opts.selection_chars = 'abcd;efgh'
