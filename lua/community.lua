e8e0972629a75296d3f88d5f83c3b7c377dfd18a
Floris Stoica <floris@nusunt.eu>
2024-04-05 14:45:02 +0300

    set title

--- lua/community.lua
+++ lua/community.lua
@@ -7,51 +7,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.lsp.lsp-signature-nvim", enabled = true },
-  {
-    import = "astrocommunity.motion.harpoon",
-    enabled = true,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      { "AstroNvim/astroui", opts = { icons = { Harpoon = "󱡀" } } },
-      {
-        "AstroNvim/astrocore",
-        opts = function(_, opts)
-          local maps = opts.mappings
-          local term_string = vim.fn.exists "$TMUX" == 1 and "tmux" or "term"
-          local prefix = "<Leader>m"
-          maps.n[prefix] = { desc = require("astroui").get_icon("Harpoon", 1, true) .. "Harpoon" }
-
-          maps.n[prefix .. "a"] = { function() require("harpoon"):list():append() end, desc = "Add file" }
-          maps.n[prefix .. "e"] = {
-            function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,
-            desc = "Toggle quick menu",
-          }
-          maps.n["<C-x>"] = {
-            function()
-              vim.ui.input({ prompt = "Harpoon mark index: " }, function(input)
-                local num = tonumber(input)
-                if num then require("harpoon"):list():select(num) end
-              end)
-            end,
-            desc = "Goto index of mark",
-          }
-          maps.n["<C-p>"] = { function() require("harpoon"):list():prev() end, desc = "Goto previous mark" }
-          maps.n["<C-n>"] = { function() require("harpoon"):list():next() end, desc = "Goto next mark" }
-          maps.n[prefix .. "m"] = { "<Cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" }
-          maps.n[prefix .. "t"] = {
-            function()
-              vim.ui.input({ prompt = term_string .. " window number: " }, function(input)
-                local num = tonumber(input)
-                if num then require("harpoon").term.gotoTerminal(num) end
-              end)
-            end,
-            desc = "Go to " .. term_string .. " window",
-          }
-        end,
-      },
-    },
-    opts = { prefix = "<Leader>m" },
-  },
+  { import = "astrocommunity.motion.harpoon", enabled = true },
+  { import = "astrocommunity.motion.leap", enabled = true },
   -- import/override with your plugins folder
 }
--- lua/polish.lua
+++ lua/polish.lua
@@ -3,23 +3,25 @@
 -- fit in the normal config locations above can go here
 
 -- Set up custom filetypes
-vim.filetype.add({
+vim.filetype.add {
   extension = {
-    c3 = "c3"
+    c3 = "c3",
   },
   pattern = {
-    [".*.blade.php"] = 'blade',
-    [".*.norg"] = 'norg',
-    ["*.c3"] = 'c3'
-  }
-})
+    [".*.blade.php"] = "blade",
+    [".*.norg"] = "norg",
+    ["*.c3"] = "c3",
+  },
+}
 -- vim.treesitter.language.register('html', 'blade')
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.blade = {
   install_info = {
     url = "https://github.com/EmranMR/tree-sitter-blade",
-    files = {"src/parser.c"},
+    files = { "src/parser.c" },
     branch = "main",
   },
-  filetype = "blade"
+  filetype = "blade",
 }
+
+vim.opt.titlestring = [[%{v:progname}: %p]]
