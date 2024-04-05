c9712ece370bdabbee647fc79b7374d36c2d28a7
Floris Stoica <floris@nusunt.eu>
2024-04-05 19:56:45 +0300

    add darcula dark

--- lua/plugins/astrotheme.lua
+++ lua/plugins/astrotheme.lua
@@ -7,7 +7,7 @@ return {
       astrodark = {
         ui = {
           prompt = "#282a36",
-          statusline = "#3B4252",
+          statusline = "#282a36",
         },
         term = {
           background = "#282a36",
