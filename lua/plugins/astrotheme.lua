7f063aa7fb9e206855409c265c997871003104d2
Floris Stoica <floris@nusunt.eu>
2024-04-05 19:59:37 +0300

    add darcula dark

--- lua/plugins/astrotheme.lua
+++ lua/plugins/astrotheme.lua
@@ -8,6 +8,9 @@ return {
         ui = {
           prompt = "#282a36",
           statusline = "#282a36",
+          tabline = "#282a36",
+          winbar = "#282a36",
+          text_match = "#ff79c6",
         },
         term = {
           background = "#282a36",
