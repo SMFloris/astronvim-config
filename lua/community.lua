caa9126c39cb82185ba2e632bcb996601531abdf
Floris Stoica <floris@nusunt.eu>
2024-04-06 20:27:08 +0300

    add neoscroll

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,7 @@ return {
   { import = "astrocommunity.colorscheme.dracula-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  { import = "astrocommunity.scrolling.cinnamon-nvim" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.lsp.lsp-signature-nvim", enabled = true, opts = { hint_enable = true } },
   { import = "astrocommunity.motion.harpoon", enabled = true },
